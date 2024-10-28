9295be43a3c7857b1fd73483da53d88b3cd24601
Rosiele Sousa <rosielesousa49@gmail.com>
2024-10-28 16:55:36 -0300

    envio dos códigos

---
+++ script.js
@@ -0,0 +1,15 @@
+const property = []
+const menu = prompt(`aqui voce cadastra seu imovel. Em que posso ajudar? escolha uma das opcoes:
+    imoveis cadastrados: ${propety.length}
+    1. salvar um novo imovel
+    2.listar os moveis salvos
+    3. mostrar os imoveis cadastrados
+    4. sair`)
+
+   switch(menu) {
+    case '1':
+        const newProperty = prompt('Qual nome do propietario?' )
+   } 
+
+
+
